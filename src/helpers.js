2d65fca86c7e1dc4238c90b9566050bcf10a3125
Guo <espana1210@gmail.com>
2022-01-26 20:06:41 -0800

    ES2015_exercise

---
+++ src/helpers.js
@@ -0,0 +1,13 @@
+function choice(items) {
+    let idx = Math.floor(Math.random()* items.length);
+    return items[idx];
+}
+
+function remove(item, items){
+    for(let i = 0; i < items.length; i++){
+        if(items[i] === item){
+            return [...items.slice(0, i), ...items.slice(i + 1)]
+        }
+    }
+}
+export default {choice, remove}
